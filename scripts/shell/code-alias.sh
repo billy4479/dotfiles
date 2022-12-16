c8e73693cb989e3a2963c26b005ca7e7d4564b83
billy4479 <giachi.ellero@gmail.com>
2022-12-16 11:14:51 +0100

    Simplify `c` alias and setup java in vscode

--- scripts/shell/code-alias.sh
+++ scripts/shell/code-alias.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
 cdc() {
-	cd $code
-	if [ "$1" != "" ]; then
-		cd "$1"
-	fi
+    cd $code
+    if [ "$1" != "" ]; then
+        cd "$1"
+    fi
 }
 
-c() {
-	if [ -z ${1+x} ]; then
-		codium .
-		return
-	fi
+#c() {
+#	if [ -z ${1+x} ]; then
+#		codium .
+#		return
+#	fi
+#
+#	if [ -d "$code/$1" ]; then
+#		cd $code/$1
+#		codium .
+#	else
+#		echo -n "Directory not found! Try on github? (y/N) "
+#		read answer
+#		if [ "$answer" = "y" ]; then
+#			cd $code
+#			git clone "git@github.com:billy4479/$1" && cd "$1" && codium .
+#		fi
+#	fi
+#}
 
-	if [ -d "$code/$1" ]; then
-		cd $code/$1
-		codium .
-	else
-		echo -n "Directory not found! Try on github? (y/N) "
-		read answer
-		if [ "$answer" = "y" ]; then
-			cd $code
-			git clone "git@github.com:billy4479/$1" && cd "$1" && codium .
-		fi
-	fi
-}
+alias c=codium
 
 alias license='license -name "Giacomo Ellero"'
