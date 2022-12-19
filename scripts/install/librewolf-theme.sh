fa4d27ac0eb363c7deb92df6ef8c4f5c22b589ae
billy4479 <giachi.ellero@gmail.com>
2022-12-19 19:38:54 +0100

    Better installation for firefox theme

--- scripts/install/librewolf-theme.sh
+++ scripts/install/librewolf-theme.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-profile_name=$(grep Default .librewolf/profiles.ini | awk -F= '{print $2}')
+profile_name=$(grep Default .librewolf/profiles.ini | grep release | awk -F= '{print $2}')
 profile_path="$HOME/.librewolf/$profile_name"
 
-download_url=$(curl -Ss "https://api.github.com/repos/black7375/Firefox-UI-Fix/releases/latest" | jq -r '.assets[].browser_download_url' | grep Lepton.zip)
-temp_zip=$(mktemp --tmpdir XXXXXXXXXX.zip)
-temp_ext_dir=$(mktemp --tmpdir -d)
+echo "$profile_path"
 
-wget "$download_url" -O "$temp_zip"
-unzip "$temp_zip" -d "$temp_ext_dir"
+pushd "${profile_path}"
 
-mv "$temp_ext_dir/chrome" "$profile_path"
-mv "$temp_ext_dir/user.js" "$profile_path"
+ln -sf ../../src/Firefox-UI-Fix chrome
+ln -sf ../../src/Firefox-UI-Fix/user.js .
+
+popd
+
