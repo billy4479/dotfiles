85a4d3f03983785ca02045a74149e23c1e670f25
billy4479 <giachi.ellero@gmail.com>
2023-03-05 11:22:17 +0100

    Laptop updates

--- .local/share/antidot/alias.sh
+++ .local/share/antidot/alias.sh
@@ -1,3 +1,3 @@
 alias gdb="gdb -n -x $XDG_CONFIG_HOME/gdb/init"
-alias wget="wget --hsts-file="${XDG_CACHE_HOME}/wget-hsts""
 alias nvidia-settings="nvidia-settings --config=${XDG_CONFIG_HOME}/nvidia/settings"
+alias wget="wget --hsts-file="${XDG_CACHE_HOME}/wget-hsts""
