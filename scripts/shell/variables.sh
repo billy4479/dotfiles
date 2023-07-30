bb22a69f65ae82cc24bcfe2589b297d9575895f5
billy4479 <giachi.ellero@gmail.com>
2023-07-31 01:39:41 +0200

    Updates!

--- scripts/shell/variables.sh
+++ scripts/shell/variables.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-export XDG_DATA_HOME=${XDG_DATA_HOME:="$HOME/.local/share"}
-export XDG_CACHE_HOME=${XDG_CACHE_HOME:="$HOME/.cache"}
-export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:="$HOME/.config"}
+export XDG_DATA_HOME="${XDG_DATA_HOME:="$HOME/.local/share"}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:="$HOME/.cache"}"
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:="$HOME/.config"}"
 
 export PNPM_HOME="$HOME/.local/share/pnpm"
 export CARGO_HOME="$HOME/.local/share/cargo"
 export JAVA_HOME="/usr/lib/jvm/default"
-export GOPATH="$HOME/go"
+# export GOPATH="$HOME/.local/share/go" # antidot does that
 export npm_config_prefix="$HOME/.local"
 
 export PATH="$HOME/.local/bin:$CARGO_HOME/bin:$GOPATH/bin:$HOME/Games/bin:$PNPM_HOME:$PATH"
@@ -18,6 +18,8 @@ export GOPROXY="direct"
 
 # Pager for Git - Disabled since it messes up with delta
 # export PAGER="bat"
+
+export SYSTEMD_PAGER="bat"
 
 export EDITOR="nvim"
 export DIFFPROG="nvim -d"
@@ -34,7 +36,7 @@ set +o noclobber
 export code="$HOME/code"
 
 # Minecraft servers folder
-export servers='/mnt/SSDSata/Server Minecraft'
+export servers="$HOME/Games/Server Minecraft"
 
 export VPN_SERVERS="$HOME/NordVPN Servers"
 
